97850a1aa4843a459cc4b4e60bfb377b68e3d011
Félix Piédallu <felix@piedallu.me>
2018-04-10 00:11:04 +0200

    fix endlines

--- demo.v
+++ demo.v
@@ -31,26 +31,26 @@ module demo(
    assign inB=PMOD2;
    assign runstop=PMOD3;
    assign reset=PMOD4;
-   
+
 
 // Carry from addition
    wire   carry;
-   
+
 // remember carry result
    reg 	  carrylatch;
-   
 
-   assign LED2=carry; 
 
-   
-   always 
+   assign LED2=carry;
+
+
+   always
      begin
 	LED1<=inA^inB;   // 1/2 adder with carry output
 	carry<=inA&inB;  // we use carry in more than one place
      end
 
    assign LED3 = carrylatch;
-   
+
 // latch carry result
    always @(posedge clk)
      begin
@@ -60,13 +60,13 @@ module demo(
 	end
      end
 
-   
-	
+
+
 
 // The 12MHz clock is too fast
 // The first counter will / 2^16 (about 5.46ms)
 // Then the second counter goes to 91 which is 0.497s
-// close enough to 1/2 second for our purpose  
+// close enough to 1/2 second for our purpose
 	always@(posedge clk)
 	  begin
 	     if (reset==1'b1)
@@ -79,22 +79,22 @@ module demo(
 	     else if (runstop==1'b0)  // don't do anything unless enabled
 	       begin
 		  cnt1 <= cnt1 + 1;
-		  if (cnt1 == 0) 
-		    if (cnt2 == 91) 
+		  if (cnt1 == 0)
+		    if (cnt2 == 91)
 		      begin
 			 cnt2 <= 0;
-			 half_sec_pulse <= 1;  
+			 half_sec_pulse <= 1;
 		      end
 			else
 			  cnt2 <= cnt2 + 1;
 		  else
 		    half_sec_pulse <= 0;
-		  
-		  if (half_sec_pulse == 1)	
+
+		  if (half_sec_pulse == 1)
 		    dec_cntr <= dec_cntr + 1; // count half seconds
 // note: dec_cntr>>1 is a seconds timer, but dec_cntr is only 2 bits so 0-3...
 	       end
-	  end	
+	  end
 
    // Make the lights blink
    assign LED4 = (dec_cntr == 2) ;
